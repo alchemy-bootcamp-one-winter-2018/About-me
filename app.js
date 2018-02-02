9b1c1a0f164ea020cbda751cd5a65a990bb471de
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-02-02 09:39:27 -0800

    use parameters to accept pet name instead of global var

--- app.js
+++ app.js
@@ -1,33 +1,47 @@
 'use strict';
 
-function havePets() {
-    const intro = prompt('Have you ever had a pet?').toLowerCase();
-    if (intro === 'yes' || intro === 'y') {
-        alert('That\'s awesome! Pet\'s are so rewarding.');
-    } else if (intro === 'no' || intro === 'n') {
-        alert('I am so sorry that you have never known the joy!');
-    } else {
-        alert('What now?');
-    }
-    console.log(intro);
+askOwnPets();
+
+const pet = getPetType();
+console.log(pet);
+
+commonPets(pet);
+
+const breed = petBreed();
+console.log(breed);
+
+pureBreed();
+
+const name = petName();
+console.log(name);
+
+funkyName(pet, breed, name);
+
+petTrips();
+
+let result = 0;
+beachTrips();
+
+petKinds();
+
+if (result == 1) {
+    alert('You got ' + result + ' out of 2 questions. Not bad.');
+} else if (result == 2) {;
+    alert('You got ' + result + ' out of 2! You know me so well.');
 }
 
-havePets();
 
-function petType(){
+function getPetType(){
     const idealPet = prompt('What is your ideal pet?');
     alert(idealPet + ' huh? Those are pretty cool. I\'m a dog person myself.');
     console.log('Their ideal pet is: ' + idealPet);
     return idealPet;
 }
 
-const pet = petType();
-console.log(pet);
-
-function commonPets() {
+function commonPets(theirPet) {
     const common = prompt('Are those common where you live?').toLowerCase();
     if (common === 'yes' || common === 'y') {
-        alert('Thought so, I can\'t imagine a place without a friendly ' + pet + ' around.');
+        alert('Thought so, I can\'t imagine a place without a friendly ' + theirPet + ' around.');
     } else if (common === 'no' || common === 'n') {
         alert('How interesting!');
     } else {
@@ -36,8 +50,6 @@ function commonPets() {
     console.log(common);
 }
 
-commonPets();
-
 function petBreed() {
     const whatBreed = prompt('What is the breed of your ' + pet + '?');
     alert('A ' + whatBreed + '? Sounds gorgeous! I\'ve never heard of a ' + pet + ' like that before.');
@@ -45,8 +57,19 @@ function petBreed() {
     return whatBreed;
 }
 
-const breed = petBreed();
-console.log(breed);
+
+function funkyName(theirPet, petBreed, petName) {
+    const funky = prompt('Are you a fan of funky pet names?').toLowerCase();
+    if (funky === 'yes' || funky === 'y') {
+        alert('There are some pretty clever ones out there and some equally silly ones. I knew a dog once called Chunky Chew.');
+    } else if (funky === 'no' || funky === 'n') {
+        alert('Well, with a ' + theirPet + ' named ' + petName + ' you seem like a more mature pet namer.');
+    } else {
+        alert('What now?');
+    }
+    console.log(funky);
+}
+
 
 function pureBreed() {
     const pure = prompt('Are you dead set on a pure breed?').toLowerCase();
@@ -60,31 +83,6 @@ function pureBreed() {
     console.log(pure);
 }
 
-pureBreed();
-
-function petName() {
-    const whatName = prompt('What would you name your ' + breed + '?');
-    alert('You would name your ' + pet + ' ' + whatName + '? Well, that\'s all right. To each their own.');
-    console.log('Their pet\'s name is: ' + whatName);
-    return whatName;
-}
-
-const name = petName();
-console.log(name);
-
-function funkyName() {
-    const funky = prompt('Are you a fan of funky pet names?').toLowerCase();
-    if (funky === 'yes' || funky === 'y') {
-        alert('There are some pretty clever ones out there and some equally silly ones. I knew a dog once called Chunky Chew.');
-    } else if (funky === 'no' || funky === 'n') {
-        alert('Well, with a ' + pet + ' named ' + name + ' you seem like a more mature pet namer.');
-    } else {
-        alert('What now?');
-    }
-    console.log(funky);
-}
-
-funkyName();
 
 function petTrips() {
     const trip = prompt('Would you consider taking your ' + pet + ' on trips to faraway places?').toLowerCase();
@@ -98,9 +96,39 @@ function petTrips() {
     console.log(trip);
 }
 
-petTrips();
+function petKinds() {
+    const myPets = ['snake','cat','hampster'];
+    for (let count = 1; count <= 6; count++) {
+        const guess = prompt('Can you guess what other pet\'s I have or have had? Hint: They definately would not have liked the beach.');
+        if (myPets.includes(guess)) {
+            console.log(guess + ' was one of my current/previous pets');
+            alert('Good Work! Right now I have a snake named Opalescence, a cat named Lynx, and an evil hampster named Chomper. Bet you can\'t guess why.');
+            result++;
+            break;
+        } else if (count > 5) {
+            console.log('Did not guess my current/previous pets');
+            alert('You were so close on a few! Right now I have a snake named Opalescence. I once had a cat named Lynx and an evil hampster named Chomper. Bet you can\'t guess why.');
+            break;
+        } else if (myPets.indexOf(guess) == -1) {
+            console.log(guess + ' was not one of my current/previous pets');
+            alert('Sorry, I\'ve never had one of those before. You have made ' + count + ' of 6 guesses!');
+        }
+    }
+}
 
-let result = 0;
+
+function askOwnPets() {
+    const intro = prompt('Have you ever had a pet?').toLowerCase();
+    if (intro === 'yes' || intro === 'y') {
+        alert('That\'s awesome! Pet\'s are so rewarding.');
+    } else if (intro === 'no' || intro === 'n') {
+        alert('I am so sorry that you have never known the joy!');
+    } else {
+        alert('What now?');
+    }
+    console.log(intro);
+}
+
 
 function beachTrips() {
     for (let i = 0; i < 4; i++) {
@@ -123,32 +151,10 @@ function beachTrips() {
     }
 }
 
-beachTrips();
 
-function petKinds() {
-    const myPets = ['snake','cat','hampster'];
-    for (let count = 1; count <= 6; count++) {
-        const guess = prompt('Can you guess what other pet\'s I have or have had? Hint: They definately would not have liked the beach.');
-        if (myPets.includes(guess)) {
-            console.log(guess + ' was one of my current/previous pets');
-            alert('Good Work! Right now I have a snake named Opalescence, a cat named Lynx, and an evil hampster named Chomper. Bet you can\'t guess why.');
-            result++;
-            break;
-        } else if (count > 5) {
-            console.log('Did not guess my current/previous pets');
-            alert('You were so close on a few! Right now I have a snake named Opalescence. I once had a cat named Lynx and an evil hampster named Chomper. Bet you can\'t guess why.');
-            break;
-        } else if (myPets.indexOf(guess) == -1) {
-            console.log(guess + ' was not one of my current/previous pets');
-            alert('Sorry, I\'ve never had one of those before. You have made ' + count + ' of 6 guesses!');
-        }
-    }
-}
-
-petKinds();
-
-if (result == 1) {
-    alert('You got ' + result + ' out of 2 questions. Not bad.');
-} else if (result == 2) {;
-    alert('You got ' + result + ' out of 2! You know me so well.');
+function petName() {
+    const whatName = prompt('What would you name your ' + breed + '?');
+    alert('You would name your ' + pet + ' ' + whatName + '? Well, that\'s all right. To each their own.');
+    console.log('Their pet\'s name is: ' + whatName);
+    return whatName;
 }
